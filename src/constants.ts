2eb52d019485fae0076fefb837bc63a6f2c21697
Sergei Polishsuk <sepolish@student.jyu.fi>
2021-09-19 06:56:59 +0300

    searchPlayers.ts, time.ts to TS

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,7 @@ export const COH2_ORG = 'https://www.coh2.org/ladders/playercard/steamid/';
 
 // steam
 export const STEAM = 'https://steamcommunity.com/profiles/';
+
+// relic
+export const RELIC_SERVER_BASE =
+    'https://coh2-api.reliclink.com/community/leaderboard';
---
+++ src/functions/searchPlayers.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+import { RELIC_SERVER_BASE } from '../constants';
+
+type Result = {
+    status: number;
+    data: any;
+};
+type Player = {
+    id: number;
+    type: number;
+    name: string;
+    members: any[];
+};
+
+export default function searchPlayers(
+    searchValue: string,
+    callback: (res: any[]) => void
+) {
+    const url = `${RELIC_SERVER_BASE}/GetPersonalStat?title=coh2&search=${searchValue}`;
+
+    axios
+        .get(url)
+        .then((result: Result) => {
+            if (
+                result.status === 200 &&
+                result.data.result.message === 'SUCCESS'
+            ) {
+                let playerArr: Player[] = result.data.statGroups.filter(
+                    (p: Player) => p.type === 1
+                );
+
+                playerArr = playerArr.map((p: Player) => p.members[0]);
+                callback(playerArr);
+            } else {
+                console.log('searchPlayers else:', result);
+                callback([]);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            callback([]);
+        });
+}
