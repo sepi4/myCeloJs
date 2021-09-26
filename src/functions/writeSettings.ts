fd3b97a38aa369651532bac4853ed9b7824f0254
Sergei Polishsuk <sepolish@student.jyu.fi>
2021-09-26 17:38:52 +0300

    writeSettings.ts to TS

---
+++ src/functions/writeSettings.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import e from 'electron';
+import { Settings } from '../types';
+
+const settingsDir = e.remote.app.getPath('userData');
+
+type D = ({ type, data}: {type: string; data: Settings}) => void;
+
+function writeSettings(
+    newSettings: Settings,
+    dispatch: D,
+) {
+    fs.writeFile(
+        settingsDir + '/settings.json',
+        JSON.stringify(newSettings, null, 4),
+        'utf-8',
+        () => {
+            dispatch({
+                type: 'SET_SETTINGS',
+                data: newSettings,
+            });
+        }
+    );
+}
+
+export default writeSettings;
