bb6047e71dbf86954c24998f64ff565296365aef
Sergei Polishsuk <polser1@gmail.com>
2026-03-05 04:40:40 +0200

    chore: update react to 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
 import { Provider } from 'react-redux'
@@ -6,9 +6,9 @@ import store from './store'
 
 import './index.css'
 
-render(
+const root = createRoot(document.getElementById('root')!)
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 )
