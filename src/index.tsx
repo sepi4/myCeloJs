b6fb893e43bc4f866ebd9272966df5094e1dc965
Sergei Polishsuk <polser1@gmail.com>
2026-03-06 18:51:38 +0200

    chore: remove Redux completely
    All state migrated to Zustand. Remove store.ts, reducer.ts,
    initialStore.ts, customReduxHooks.ts, Provider wrapper, Store type,
    and uninstall redux/react-redux/@reduxjs/toolkit.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-import { Provider } from 'react-redux'
-import store from './store'
-
 import './index.css'
 
 const root = createRoot(document.getElementById('root')!)
-root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
+root.render(<App />)
