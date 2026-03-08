ee72fad2c4f0bdc40549817b2c7b9ffdd4e555a3
Sergei Polishsuk <polser1@gmail.com>
2026-03-08 08:48:24 +0200

    test: add reporter to e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,4 +4,9 @@ export default defineConfig({
     testDir: './e2e',
     timeout: 30000,
     workers: 1,
+    reporter: 'html',
+    use: {
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
+    },
 })
