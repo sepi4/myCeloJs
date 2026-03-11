cbfec911846f4c30b06f0b381f79bfc26238fc1e
Sergei Polishsuk <polser1@gmail.com>
2026-03-11 04:11:35 +0200

    chore: add coverage script scoped to functions and stores

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     // testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     testRegex: '/src/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverageFrom: ['src/functions/**/*.ts', 'src/stores/**/*.ts', '!src/**/*.d.ts'],
+    coverageProvider: 'v8',
+    coverageReporters: ['text', 'html'],
 }
