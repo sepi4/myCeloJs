47337e6f6d3f5c9b32b365da07a64762ac0b0c26
Sergei Polishsuk <polser1@gmail.com>
2026-03-19 09:37:12 +0200

    refactor: improve readability of renderRankingsHtml

--- src/functions/renderRankingsHtml.ts
+++ src/functions/renderRankingsHtml.ts
@@ -8,30 +8,52 @@ function escapeHtml(str: string): string {
         .replace(/"/g, '&quot;')
 }
 
+function img(src: string): string {
+    return `<img src="${src}" width="100%" height="100%" />`
+}
+
 function renderPlayer(
     player: { name: string; ranking: string; country?: string; faction: string },
     reversed: boolean
 ): string {
-    const factionImg = `<div class="factionStyle ${reversed ? 'marginRight3' : 'marginLeft3'}"><img src="/img/${escapeHtml(player.faction)}.png" width="100%" height="100%" /></div>`
-    const rankSpan = `<span class="rankingStyle">${escapeHtml(player.ranking)}</span>`
-    const countryDiv = `<div class="countryStyle">${player.country ? `<img src="/img/countryFlags/${escapeHtml(player.country)}.png" width="100%" height="100%" />` : ''}</div>`
-    const nameSpan = `<span class="nameStyle ${reversed ? 'textRight marginRight1' : 'marginLeft1'}">${escapeHtml(player.name)}</span>`
+    const faction = escapeHtml(player.faction)
+    const name = escapeHtml(player.name)
+    const ranking = escapeHtml(player.ranking)
+    const country = player.country ? escapeHtml(player.country) : ''
 
-    if (reversed) {
-        return `<div class="playerStyle reversed">${nameSpan}${countryDiv}${rankSpan}${factionImg}</div>`
-    }
-    return `<div class="playerStyle">${factionImg}${rankSpan}${countryDiv}${nameSpan}</div>`
+    const factionMargin = reversed ? 'marginRight3' : 'marginLeft3'
+    const nameMargin = reversed ? 'textRight marginRight1' : 'marginLeft1'
+
+    const factionDiv = `<div class="factionStyle ${factionMargin}">${img(`/img/${faction}.png`)}</div>`
+    const rankSpan = `<span class="rankingStyle">${ranking}</span>`
+    const countryDiv = `<div class="countryStyle">${country ? img(`/img/countryFlags/${country}.png`) : ''}</div>`
+    const nameSpan = `<span class="nameStyle ${nameMargin}">${name}</span>`
+
+    const parts = reversed
+        ? [nameSpan, countryDiv, rankSpan, factionDiv]
+        : [factionDiv, rankSpan, countryDiv, nameSpan]
+
+    const cls = reversed ? 'playerStyle reversed' : 'playerStyle'
+    return `<div class="${cls}">${parts.join('')}</div>`
 }
 
 export function renderRankingsBody(data: RankingsJson): string {
     const h = data.horizontal
+    const suffix = h ? 'Horizontal' : ''
+
     const team1 = data.teams.team1.map((p) => renderPlayer(p, h)).join('')
     const team2 = data.teams.team2.map((p) => renderPlayer(p, false)).join('')
 
-    return `<div class="bodyStyle${h ? 'Horizontal' : ''}"><div class="teamStyle${h ? 'Horizontal' : ''}">${team1}</div><div class="teamStyle${h ? 'Horizontal' : ''}">${team2}</div></div>`
+    return [
+        `<div class="bodyStyle${suffix}">`,
+        `<div class="teamStyle${suffix}">${team1}</div>`,
+        `<div class="teamStyle${suffix}">${team2}</div>`,
+        `</div>`,
+    ].join('')
 }
 
-const CSS = `* {
+const CSS = `
+* {
     margin: 0;
     padding: 0;
     font-family: 'Work Sans', 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
@@ -96,7 +118,8 @@ const CSS = `* {
 }
 .marginRight3 {
     margin-right: 3em;
-}`
+}
+`
 
 export function renderRankingsPage(): string {
     return `<!doctype html>
