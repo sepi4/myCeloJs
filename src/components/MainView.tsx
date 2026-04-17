f4eeaaa38b8f83a033dfa21ce6ac0402c5c6a562
Sergei Polishsuk <polser1@gmail.com>
2026-04-17 03:04:03 +0300

    feat: show teams side by side when window is wide enough
    Use flex layout with a media query at 1200px to switch from
    stacked (column) to side-by-side (row) team display.

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -63,7 +63,7 @@ function MainView(props: { handleSetSettingsView: () => void }): JSX.Element {
 
     if (players && players.length > 0) {
         return (
-            <div data-testid="players-container">
+            <div data-testid="players-container" className={styles.teamsContainer}>
                 <Team players={teams[0]} teamIndex={0} />
                 <Team players={teams[1]} teamIndex={1} />
             </div>
